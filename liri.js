ebca9e34a8733ede2598f14b1915fd5df19e5966
Jegors Jeskovs <jegorsj@gmail.com>
2019-10-01 21:13:40 -0600

    created the file stracture

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
